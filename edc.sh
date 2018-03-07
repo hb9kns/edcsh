aac35612a9a532640f288304de87666908c0b39b
Yargo iMac <yargo+git-imac@phys.ethz.ch>
2018-03-07 13:24:12 +0100

    trials

--- edc.sh
+++ edc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-info='edc.sh // 2018-3-6 Y.Bonetti // https://github.com/hb9kns/edcsh'
+info='edc.sh // 2018-3-7 Y.Bonetti // https://github.com/hb9kns/edcsh'
 conf=${EDCRC:-$HOME/.edcrc}
 edcp=${TMPDIR:-/tmp}/edcpipe$$
 if test "$1" = ""
@@ -14,20 +14,40 @@ EOH
  exit 0
 fi
 
+edbin=/bin/ed
+edprompt=:
+edopts=-p$edprompt
 edeprompt=::
+edcprompt=edc:
 ede () { echo $edeprompt "$*" >&2 ; }
 
-if ! mkfifo $edcp
+if ! mkfifo -m 600 $edcp
 then ede cannot mkfifo $edcp
  exit 1
 else ede opened edcpipe:
- ls -l $edcp
+ ede `ls -l $edcp`
 fi
 
 if test -r "$conf"
 then ede sourcing config file "$conf"
  . "$conf"
-else ede default config due to unreadable config file "$conf"
+else ede using defaults due to unreadable file "$conf"
 fi
+
+doed () {
+ $edbin $edopts "$@" <$edcp
+}
+
+doed "$filen" &
+pided=$!
+ps -p $pided | grep -F "$pided"
+
+while ( ps -p $pided | grep -F "$pided" >/dev/null 2>&1 )
+do read -p "$edcprompt" c1 cr
+ case $c1 in
+ Quit) echo 'q!' >>$edcp ;;
+ *) echo "$c1 $cr" >>$edcp ;;
+ esac
+done
 
 rm -f $edcp
