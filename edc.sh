dcea17dbf790acda6394ea2ba37988ea1887b3a8
Yargo AAOne <yargo+git-aao@jerq.org>
2018-03-08 00:39:48 +0000

    use tail -f on FIFO and piping to ed, instead of piping FIFO to ed

--- edc.sh
+++ edc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-info='edc.sh // 2018-3-7 Y.Bonetti // https://github.com/hb9kns/edcsh'
+info='edc.sh // 2018-3-8 Y.Bonetti // https://github.com/hb9kns/edcsh'
 conf=${EDCRC:-$HOME/.edcrc}
 edcp=${TMPDIR:-/tmp}/edcpipe$$
 if test "$1" = ""
@@ -14,12 +14,14 @@ EOH
  exit 0
 fi
 
+DEBUG=
 edbin=/bin/ed
 edprompt=:
 edopts=-p$edprompt
 edeprompt=::
 edcprompt=edc:
 ede () { echo $edeprompt "$*" >&2 ; }
+edd () { if test -n "$DEBUG" ; then echo $edeprompt DEBUG "$*" >&2 ; fi ; }
 
 if ! mkfifo -m 600 $edcp
 then ede cannot mkfifo $edcp
@@ -28,32 +30,36 @@ else ede opened edcpipe:
  ede `ls -l $edcp`
 fi
 
-tail -f /dev/null > $edcp &
-tailpid=$!
-
 if test -r "$conf"
 then ede sourcing config file "$conf"
  . "$conf"
 else ede using defaults due to unreadable file "$conf"
 fi
 
-$edbin $edopts "$@" < $edcp &
+tail -s 0.2 -f $edcp | $edbin $edopts "$@" &
 pided=$!
+edd pided=$pided
 
-echo H > $edcp
-ps -p $pided | grep -F "$pided"
+if test -n "$DEBUG"
+then
+ echo H > $edcp
+ ps -p $pided | grep -F "$pided"
+fi
 
-while ps -p $pided | grep -F "$pided" >/dev/null 2>&1
+while ps -p $pided >/dev/null 2>&1
 do
+if test -n "$DEBUG"
+then
  ps -p $pided | grep -F "$pided"
  printf '%s' "$?-$edcprompt"
+fi
  read c1 cr
  case $c1 in
  Quit) echo 'q!' > $edcp
-  kill -9 $tailpid
+  kill -9 $pided
   ;;
- *) ede "c1=$c1 cr=$cr"
-  echo "$c1 $cr" > $edcp ;;
+ *) edd "c1=$c1 cr=$cr"
+  echo $c1 $cr > $edcp ;;
  esac
 done
 
