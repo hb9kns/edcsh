f6c88731a8030a4fabedb52b98ac6df3b1268787
YargoA2 <yargo+git-asus2@jerq.org>
2018-03-09 09:08:40 +0100

    begin pi(pe) command

--- edc.sh
+++ edc.sh
@@ -3,6 +3,9 @@ DEBUG=
 info='edc.sh // 2018-3-8 Y.Bonetti // https://github.com/hb9kns/edcsh'
 conf=${EDCRC:-$HOME/.edcrc}
 edcp=${TMPDIR:-/tmp}/edcpipe$$
+tmp0=${TMPDIR:-/tmp}/edctmp$$-0
+tmp1=${TMPDIR:-/tmp}/edctmp$$-1
+tmp2=${TMPDIR:-/tmp}/edctmp$$-2
 if test "$1" = ""
 then cat <<EOH
 usage: $0 file
@@ -35,15 +38,16 @@ then ede sourcing config file "$conf"
 else ede using defaults due to unreadable file "$conf"
 fi
 
-doed () { tail -s 0.2 -f $edcp | $edbin $edopts "$@" ; }
+runed () { tail -s 0.2 -f $edcp | $edbin $edopts "$@" ; }
+edex () { echo "$@" > $edcp ; }
 
-doed "$@" &
+runed "$@" &
 pided=$!
 edd pided=$pided
 
 if test -n "$DEBUG"
 then
- echo H > $edcp
+ edex H
  ps -p $pided | grep -F "$pided"
 fi
 
@@ -54,13 +58,27 @@ do
  case $cl in
  help) cat <<EOH
 help for $0
+ pi[q] cmd range : pipe range through cmd, eg 'pi fmt .,.+8'
+  with q, don't display processed range
 EOH
+  ;;
+ pi*) echo "$cl" | { read _ cmd rng
+   if test -x $cmd
+   then edex %w $tmp0
+    edex $rng w $tmp1
+    edex $rng d
+    $cmd <$tmp1 >$tmp2
+    cat $tmp2
+    edex .-r $tmp2
+   else ede cannot execute $cmd
+   fi
+   }
   ;;
  *) edd cl=$cl
   if ps -p $pided >/dev/null
-  then echo "$cl" > $edcp
+  then edex "$cl"
   fi ;;
  esac
 done
 
-rm -f $edcp
+rm -f $edcp $tmp0 $tmp1 $tmp2
